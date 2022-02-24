6b68e4c120c16116709277a9a665ff4cf4737f5f
devansh <devansh191999@gmail.com>
2022-02-24 23:56:18 +0530

    Project First Commit

---
+++ src/components/About.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+const About = () => {
+  const [buttonLink, setButtonLink] = useState("");
+  const styleAbout = {
+    fontSize: "20px",
+  };
+
+  const styleName = {
+    fontFamily: "Dancing Script",
+    fontSize: "30px",
+    fontWeight: "bold",
+  };
+
+  const githubClick = () => {
+    setButtonLink("https://github.com/devansh9415");
+  };
+
+  const websiteClick = () => {
+    setButtonLink("https://www.linkedin.com/in/devansh7607/");
+  };
+
+  return (
+    <>
+      <div className="row" style={{ height: "75px" }} />
+      <div className="row" style={styleAbout}>
+        <div className="col s0 m3"> </div>
+        <div className="col s12 m6">
+          <div className="card grey lighten-3 hoverable">
+            <div className="card-content black-text">
+              <span className="card-title">
+                {" "}
+                <i className="material-icons red-text">favorite</i>
+              </span>
+              <p>App Version 1.0.0</p>
+              <p>
+                Built by{" "}
+                <span style={styleName} className="red-text">
+                  Devansh Nigam
+                </span>{" "}
+                <i className="tiny material-icons">copyright</i> 2021
+              </p>
+            </div>
+            <div className="card-action">
+              <form action={buttonLink} target="_blank">
+                <button
+                  className="btn waves-effect waves-light brown darken-1"
+                  style={{ marginRight: "10px" }}
+                  onClick={githubClick}
+                >
+                  Github
+                  <i className="small material-icons left">code</i>
+                </button>
+
+                <button
+                  className="btn waves-effect waves-light brown darken-1"
+                  onClick={websiteClick}
+                >
+                  Linkedin
+                  <i className="small material-icons right">person_add</i>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="col s0 m3"> </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
